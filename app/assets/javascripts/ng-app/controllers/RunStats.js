61bd237d309d52cd58f156852d2191586989b409
tfollo <timothy.follo@yale.edu>
2014-07-24 01:28:20 -0400

    worked on end template

--- app/assets/javascripts/ng-app/controllers/RunStats.js
+++ app/assets/javascripts/ng-app/controllers/RunStats.js
@@ -1,12 +1,12 @@
 myApp.controller('RunStatsCtrl', function ($scope, $location, $http, sharedProperties) {
       $scope.locate = sharedProperties.getRun();
     	$scope.slide = 'slide-left';
-      $scope.runInfo = {note: ""};
 
     	$http.get('/runtype.json', 
     		{ params: {id: $scope.locate.run1}}).success(function(data){
     			$scope.run = data;
-          $scope.steps = $scope.run.steps
+          $scope.steps = $scope.run.steps;
+          $scope.note = $scope.steps.splice($scope.steps.length-1);
     		})
 
        $scope.view = function(data){
