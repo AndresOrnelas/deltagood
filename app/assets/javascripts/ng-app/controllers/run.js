e57839d6c96c13f2fdcd1c90cff52288e2978d97
Henok <henok@Hannas-MacBook-Pro.local>
2014-07-24 00:16:39 -0400

    Took all of the work on making the run history route to either the current step if it is not finished or the statistics page if it is completed, back to creating a run when you hit begin with the right user and counter number

--- app/assets/javascripts/ng-app/controllers/run.js
+++ app/assets/javascripts/ng-app/controllers/run.js
@@ -14,18 +14,19 @@ myApp.controller('RunCtrl', function ($scope, $routeParams, $http, $location, sh
 
     $scope.visit = function(id){    
         sharedProperties.setRun(id);
-        $http.get('/runtype.json', {params: {id: id}}).success(function(data){
-            $scope.currentrun = data;
-        });
+        // $http.get('/runtype.json', {params: {id: id}}).success(function(data){
+        //     $scope.currentrun = data;
+        // });
 
-        if($scope.currentrun.currentStep < $scope.currentrun.inputs.length-1){
-            $location.url('/newrun'+ 
-        }
-        else{
-            $location.url('/'+ locate.protocol1 +'/'+ run.id)
+        // if($scope.currentrun.currentStep < $scope.currentrun.inputs.length-1){
+        //     $location.url('/newrun'+ 
+        // }
+        // else{
 
-        }
-        //if <100 go to steps
-        //else go to statistics
+        // }
+        // //if <100 go to steps
+        // //else go to statistics
+        $location.url('/'+ locate.protocol1 +'/'+ id)
+
     }
 });
